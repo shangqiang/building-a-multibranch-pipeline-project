46f32ec372996e534f81443d37bc60a0667a2f92
上墙 <wangshangquan@lomoment.com>
2019-12-16 11:46:07 +0800

    'test11232'

--- src/App.js
+++ src/App.js
@@ -11,7 +11,7 @@ class App extends Component {
           <h1 className="App-title">Welcome to React</h1>
         </header>
         <p className="App-intro">
-          To get started1121, edit <code>src/App.js</code> and save to reload.
+          To get started11212, edit <code>src/App.js</code> and save to reload.
         </p>
       </div>
     );
